442ff30c0ef05b6ebb80ebba4f3d1d3aeaf2d182
wualbert <wualbert@mit.edu>
2019-02-13 16:28:13 -0500

    added gitignore

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/racecar/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/melodic".split(';') if "/opt/ros/melodic" != "" else []
+underlay_workspaces = "/home/racecar/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/racecar/catkin_ws/devel;/opt/ros/melodic" != "" else []
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -117,6 +117,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/racecar/catkin_ws/build/gtest/cmake_install.cmake")
+  include("/home/racecar/catkin_ws/build/ros_exercises/cmake_install.cmake")
 
 endif()
 
